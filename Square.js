228572c31c340b91f0ff8c13043f62f2437f8e75
voffamamma <voffamamma@gmail.com>
2016-01-27 18:12:35 +0000

    Create Square.js

---
+++ Square.js
@@ -0,0 +1,30 @@
+var Square = Shape.extend({
+
+	constructor: function() {
+		this.base("Square");
+	},
+
+	draw: function(canvas) {
+		canvas.strokeStyle = this.color;
+		canvas.strokeRect(this.pos.x, this.pos.y, this.size.x, this.size.y);
+		this.base(canvas);
+	},
+
+	drawing:function(point) {
+		this.size.x = point.x - this.pos.x;
+		this.size.y = point.y - this.pos.y;
+	},
+
+	added: function(canvas) {
+		if(this.size.x < 0) {
+			this.pos.x += this.size.x;
+			this.size.x = Math.abs(this.size.x);
+		}
+
+		if(this.size.y < 0) {
+			this.pos.y += this.size.y;
+			this.size.y = Math.abs(this.size.y);
+		}
+	},	
+
+});
