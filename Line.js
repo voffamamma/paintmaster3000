cc0a98712065c8705473984bb1b5ecf16a0e29d5
stebbihalldors <stebbihalldors@gmail.com>
2016-01-31 19:49:31 +0000

    Update Line.js

--- Line.js
+++ Line.js
@@ -1,10 +1,10 @@
-
 var Line = Shape.extend({
  
     constructor: function() {
         this.base("Line");
     },
-    draw: function(canvas) {
+    
+        draw: function(canvas) {
 
         canvas.strokeStyle = this.color;
         canvas.beginPath();
@@ -14,18 +14,30 @@ var Line = Shape.extend({
         canvas.closePath();
         this.base(canvas);
     },
+    
+    startDrawing:function(point) {
+        this.size.x = this.pos.x;
+        this.size.y = this.pos.y;
+    },
+
     drawing:function(point) {
         this.size.x = point.x;// - this.pos.x;
         this.size.y = point.y;// - this.pos.y;
     },
+
+    stopDrawing:function(point) {
+        this.size.x = point.x;
+        this.size.y = point.y;
+    },
+    
     added: function(canvas) {
-        if(this.size.x < 0) {
+   /*     if(this.size.x < 0) {
             this.pos.x += this.size.x;
             this.size.x = Math.abs(this.size.x);
         }
         if(this.size.y < 0) {
             this.pos.y += this.size.y;
             this.size.y = Math.abs(this.size.y);S
-        }
+        }*/
     }, 
 }); 
